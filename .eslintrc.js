2721b74a90042991e7e47bc98bf441678ce92e82
kenobi io <io.kenobi@gmail.com>
2022-06-28 19:15:10 +0300

    [fix] config;

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,8 @@ module.exports = {
     ignorePatterns: ['.eslintrc.js'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'on',
+        '@typescript-eslint/explicit-function-return-type': 'error',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'on',
+        '@typescript-eslint/no-explicit-any': 'error',
     },
 };
